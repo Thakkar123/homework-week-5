1ce511c0ac82f09eb426eeb42492343e5ff0296a
Thakkar123 <ajit.thakkar17@gmail.com>
2022-03-07 08:54:24 +0000

    Updated homework week 5

--- src/homework/Program5_Traiangle2.java
+++ src/homework/Program5_Traiangle2.java
@@ -3,10 +3,10 @@ package homework;
 public class Program5_Traiangle2 {
     public static void main(String[] args) {
         System.out.println("     *");
-        System.out.println("    **");
-        System.out.println("   ***");
-        System.out.println("  ****");
-        System.out.println(" *****");
-        System.out.println("******");
+        System.out.println("    * *");
+        System.out.println("   * * *");
+        System.out.println("  * * * *");
+        System.out.println(" * * * * *");
+        System.out.println("* * * * * *");
     }
 }
